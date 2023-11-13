bebccaa377391c091c354abe4485bf396e9f8ab0
ANTL-KJH <wogus9190@gmail.com>
2023-11-13 15:30:22 +0900

    Update PI_Capture

--- PI_Capture.py
+++ PI_Capture.py
@@ -1,22 +1,30 @@
 import Tkinter
 import subprocess
+import os
 
-top = Tkinter.Tk()
-top.title("PI Screen Shot")
-top.geometry("300x150")
-
+def createDirectory(directory):
+    try:
+        if not os.path.exists(directory):
+            os.makedirs(directory)
 
 def capFullScreen():
-    subprocess.call(["scrot"])
-
+    subprocess.call(["scrot", "/home/pi/KJH_Capture"])
 
 def capWindowScreen():
     subprocess.call(["scrot", "-s"])
 
+def main():
+    top = Tkinter.Tk()
+    top.title("PI Screen Shot")
+    top.geometry("300x150")
 
-B = Tkinter.Button(top, text="Full Screen Capture", width=50, command=capFullScreen)
-B.pack()
-B = Tkinter.Button(top, text="Window Capture", width=50, command=capWindowScreen)
-B.pack()
 
-top.mainloop()
+    B = Tkinter.Button(top, text="Full Screen Capture", width=50, command=capFullScreen)
+    B.pack()
+    B = Tkinter.Button(top, text="Window Capture", width=50, command=capWindowScreen)
+    B.pack()
+
+    top.mainloop()
+
+if __name__ == "__main__":
+    main()
