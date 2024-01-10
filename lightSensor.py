e1133ee76f987ff9767f34682b5874e76bb13c79
ANTL-KJH <wogus9190@gmail.com>
2024-01-10 20:24:54 +0900

    Update lightSensor.py

--- lightSensor.py
+++ lightSensor.py
@@ -1,25 +1,28 @@
 import spidev, time
 
-spi = spidev.SpiDev()
 
-spi.open(0, 0)
+class photoResister:
+    def __init__(self):
+        self.spi = spidev.SpiDev()
+        spi.open(0, 0)
+        self.spi.max_speed_hz = 1350000
 
-spi.max_speed_hz = 1350000
+    def analog_read(self, channel):
+        r = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        adc_out = ((r[1] & 3) << 8) + r[2]
+        return adc_out
 
+    def runPhotoResister(self):
+        while True:
+            reading = self.analog_read(0)
+            voltage = reading * 3.3 / 1024
+            print("Reading=%d\tVoltage=%f" % (reading, voltage))
+            time.sleep(1)
 
-def analog_read(channel):
-    r = spi.xfer2([1, (8 + channel) << 4, 0])
+def main():
+    lightSensor = photoResister()
+    lightSensor.runPhotoResister()
 
-    adc_out = ((r[1] & 3) << 8) + r[2]
+if __name__ == "__main__":
+    main()
 
-    return adc_out
-
-
-while True:
-    reading = analog_read(0)
-
-    voltage = reading * 3.3 / 1024
-
-    print("Reading=%d\tVoltage=%f" % (reading, voltage))
-
-    time.sleep(1)
