21dc2d0368dae1b6e6761bf5839b1ca8ef679427
ANTL-KJH <wogus9190@gmail.com>
2024-01-12 13:19:55 +0900

    Update ledDimming.py

--- ledDimming.py
+++ ledDimming.py
@@ -4,15 +4,15 @@ import RPi.GPIO as GPIO
 class LEDDimmingController:
     def __init__(self):
         self.redLedPin = 12  # PWM0 red
-        self.redLedPin = 32  # PWM0 green
-        self.redLedPin = 33  # PWM0 blue
+        self.greenLedPin = 32  # PWM0 green
+        self.blueLedPin = 33  # PWM0 blue
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self.redLedPin, GPIO.OUT)
         self.pwmRedLed = GPIO.PWM(self.redLedPin, 500)
         self.pwmRedLed.start(100)
-        self.pwmGreenLed = GPIO.PWM(self.redLedPin, 500)
+        self.pwmGreenLed = GPIO.PWM(self.greenLedPin, 500)
         self.pwmGreenLed.start(100)
-        self.pwmBlueLed = GPIO.PWM(self.redLedPin, 500)
+        self.pwmBlueLed = GPIO.PWM(self.blueLedPin, 500)
         self.pwmBlueLed.start(100)
 
     def runLedDimming(self):
